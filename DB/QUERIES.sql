fd374fb4a50b47770a69081657c721b7b1ff63bf
dsarozz <51450527+dsarozz@users.noreply.github.com>
2020-09-11 15:07:28 +0545

    Update QUERIES.sql

--- DB/QUERIES.sql
+++ DB/QUERIES.sql
@@ -3,45 +3,31 @@ DECLARE
 TOTAL integer;
 SUBJECTCOUNT integer;
 PERCENTAGE float;
-DIVISION varchar;
+DIV varchar;
+student_id integer;
 BEGIN
 IF(TG_OP='DELETE')THEN
+student_id:=OLD.studentid;
+ELSE
+student_id:=NEW.studentid;
+END IF;
 select sum(marks),count(subjectid) into TOTAL,SUBJECTCOUNT from studentsubjects 
-where studentid= OLD.studentid and datedeleted is null group by studentid;
+where studentid= student_id and datedeleted is null group by studentid;
 PERCENTAGE :=TOTAL/SUBJECTCOUNT;
 IF PERCENTAGE>'75' 
- THEN DIVISION='DISTINCTION';
+ THEN DIV='DISTINCTION';
  ELSEIF PERCENTAGE between'60'and'75' 
- THEN DIVISION='FIRST'; 
+ THEN DIV='FIRST'; 
  ELSEIF PERCENTAGE between'50'and'60' 
- THEN DIVISION='SECOND';
+ THEN DIV='SECOND';
  ELSEIF PERCENTAGE between'40'and'50' 
- THEN DIVISION='THIRD';
+ THEN DIV='THIRD';
  ELSEIF PERCENTAGE<'40'
- THEN DIVISION='FAILED'; 
+ THEN DIV='FAILED'; 
  ELSE
- DIVISION='NO RESULT FOUND!';
+ DIV='NO RESULT FOUND!';
  END IF;
- update students set division = DIVISION, datemodified = now() where studentid=OLD.studentid;
- ELSE
- select sum(marks),count(subjectid) into TOTAL,SUBJECTCOUNT from studentsubjects 
-where studentid= NEW.studentid and datedeleted is null group by studentid;
-PERCENTAGE :=TOTAL/SUBJECTCOUNT;
-IF PERCENTAGE>'75' 
- THEN DIVISION='DISTINCTION';
- ELSEIF PERCENTAGE between'60'and'75' 
- THEN DIVISION='FIRST'; 
- ELSEIF PERCENTAGE between'50'and'60' 
- THEN DIVISION='SECOND';
- ELSEIF PERCENTAGE between'40'and'50' 
- THEN DIVISION='THIRD';
- ELSEIF PERCENTAGE<'40'
- THEN DIVISION='FAILED'; 
- ELSE
- DIVISION='NO RESULT FOUND!';
- END IF;
- update students set division = DIVISION, datemodified = now() where studentid=NEW.studentid;
- END IF;
+ update students set division = DIV, datemodified = now() where studentid=student_id;
  RETURN NULL;
 END;
 $insert_to_division$ LANGUAGE plpgsql;
@@ -52,5 +38,3 @@ AFTER
 INSERT OR UPDATE OR DELETE ON studentsubjects 
 FOR EACH ROW 
 EXECUTE PROCEDURE division_update();
-
-drop trigger insert_to_division on studentsubjects
