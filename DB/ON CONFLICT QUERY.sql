aac55d7f5c77194c009669b1107cffd2d178cccb
dsarozz <51450527+dsarozz@users.noreply.github.com>
2020-09-21 23:45:04 +0545

    Add files via upload

--- DB/ON CONFLICT QUERY.sql
+++ DB/ON CONFLICT QUERY.sql
@@ -7,15 +7,32 @@ BEGIN
 			ON CONFLICT (studentid) DO UPDATE SET studentname= students.studentname
 	RETURNING studentid
 	)
---OPTIONAL
--- ,
---temptable as (
--- SELECT studentrecord.studentid, subjectid,marks 
--- 	FROM json_populate_recordset(null::studentsubjects, (select * from json_data)::json->'subjects'), studentrecord
--- )
 	INSERT INTO studentsubjects(studentid,subjectid,marks)
 	SELECT studentrecord.studentid, subjectid, marks 
 	FROM json_populate_recordset(null::studentsubjects, json_data::json->'subjects'), studentrecord
+	ON CONFLICT ON CONSTRAINT studentsubject_unique DO UPDATE SET
+	marks=EXCLUDED.marks;
+END;
+$$ LANGUAGE plpgsql
+
+--OR
+
+CREATE OR REPLACE FUNCTION insert_json_data(json_data json)
+RETURNS void AS $$
+BEGIN
+	with studentrecord as (
+	INSERT INTO students(studentid,studentname,address)
+		SELECT studentid,studentname, address FROM json_populate_record(null::students,json_data::json)
+			ON CONFLICT (studentid) DO UPDATE SET studentname= students.studentname
+	RETURNING studentid
+	),
+temptable AS(
+SELECT studentrecord.studentid, subjectid, marks 
+	FROM json_populate_recordset(null::studentsubjects, json_data::json->'subjects'), studentrecord
+)
+	INSERT INTO studentsubjects(studentid,subjectid,marks)
+	SELECT studentid, subjectid, marks 
+	FROM temptable
 	ON CONFLICT ON CONSTRAINT studentsubject_unique DO UPDATE SET
 	marks=EXCLUDED.marks;
 END;
